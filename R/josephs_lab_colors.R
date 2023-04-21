7801f720b76e2c1b4ca326d041f59198a32ad946
Nathan <nathanscatlin@gmail.com>
2023-04-21 15:11:31 -0400

    Changing name of package to josephs_lab_colors

---
+++ R/josephs_lab_colors.R
@@ -0,0 +1,6 @@
+#' @title josephs_lab_colors
+#' @name josephs_lab_colors
+#' @docType package
+#' @details list of palettes from the Josephs Lab at Michigan State University
+#' @description list of palettes from the Josephs Lab at Michigan State University
+NULL
--- R/wesanderson.R
+++
@@ -1,6 +0,0 @@
-#' @title wesanderson
-#' @name wesanderson
-#' @docType package
-#' @details list of palettes from Wes Anderson movies
-#' @description list of palettes from Wes Anderson movies
-NULL
